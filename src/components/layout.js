c5c347c7ac4c58eba96b95e9801195d06b0b304b
Stackbit Code Editor <projects@stackbit.com>
2021-03-15 02:34:55 +0000

    layout.js: changed

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,14 @@ import Menu from './Menu'
 import Contact from './Contact'
 import Footer from './Footer'
 
+
+import pic01 from '../assets/images/pic01.jpg'
+import pic02 from '../assets/images/pic02.jpg'
+import pic03 from '../assets/images/pic03.jpg'
+import pic04 from '../assets/images/pic04.jpg'
+import pic05 from '../assets/images/pic05.jpg'
+import pic06 from '../assets/images/pic06.jpg'
+
 class Layout extends React.Component {
     constructor(props) {
         super(props)
