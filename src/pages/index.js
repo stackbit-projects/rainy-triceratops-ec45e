1e6b3a9c0836858193057aee5dbd5b3b74fa3089
Stackbit Code Editor <projects@stackbit.com>
2021-03-15 02:34:49 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,6 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Banner from '../components/Banner'
 
-import pic01 from '../assets/images/pic01.jpg'
-import pic02 from '../assets/images/pic02.jpg'
-import pic03 from '../assets/images/pic03.jpg'
-import pic04 from '../assets/images/pic04.jpg'
-import pic05 from '../assets/images/pic05.jpg'
-import pic06 from '../assets/images/pic06.jpg'
 
 class HomeIndex extends React.Component {
     render() {
